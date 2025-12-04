d44cc998a002bf23b2bacedc5210f3dcbd3a6445
ThinilSandaru <thinilsandaru@gmail.com>
2025-12-04 13:19:52 +0530

    Added the process for admins to update the status of the user

--- backend/src/main/java/com/example/backend/Repositories/FundraiserRepository.java
+++ backend/src/main/java/com/example/backend/Repositories/FundraiserRepository.java
@@ -1,6 +1,14 @@
 package com.example.backend.Repositories;
 
 import com.example.backend.Model.Fundraiser;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface FundraiserRepository extends MongoRepository<Fundraiser,String> {
+
+Optional<Fundraiser> findByUserId(ObjectId id);
+
 }
