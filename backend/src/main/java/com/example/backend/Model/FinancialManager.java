e10128610ea09b67da5c507737dc577c6e6f0bc7
ThinilSandaru <thinilsandaru@gmail.com>
2025-12-05 21:15:44 +0530

    Added the feature for the super admin to create admins and finance manager.

---
+++ backend/src/main/java/com/example/backend/Model/FinancialManager.java
@@ -0,0 +1,20 @@
+package com.example.backend.Model;
+
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "financialManager")
+public class FinancialManager {
+
+@Id
+private ObjectId id;
+
+private ObjectId userId;
+private String fullName;
+private String phoneNumber;
+
+
+}
