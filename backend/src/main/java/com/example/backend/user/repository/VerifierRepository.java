7c434339ad1be26cf668214b48a1ae9d9fcc8830
ThinilSandaru <thinilsandaru@gmail.com>
2026-05-28 20:54:08 +0530

    Created the profile fetching apis for users except the recipients

--- backend/src/main/java/com/example/backend/user/repository/VerifierRepository.java
+++ backend/src/main/java/com/example/backend/user/repository/VerifierRepository.java
@@ -3,8 +3,12 @@ package com.example.backend.user.repository;
 import com.example.backend.user.model.Verifier;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface VerifierRepository extends MongoRepository<Verifier,String> {
 
 	void deleteByUserId(String userId);
+
+	Optional<Verifier> findByUserId(String userId);
 
 }
