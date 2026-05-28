a303a0fd89cc14773d8c0399516539043fc3505e
HeshanSandeepanaJayasinghe <heshansandeepanajayasinghe@gmail.com>
2026-05-28 13:04:52 +0530

    Statistics initiated + small fixes

--- frontend/src/pages/Statistics.jsx
+++ frontend/src/pages/Statistics.jsx
@@ -1,1 +1,13 @@
 //Statistics for admin and superadmin
+import React from 'react';
+import './Statistics.css';
+
+const Statistics = () => {
+  return (
+  <div className="container">
+    
+  </div>
+  );
+};
+
+export default Statistics;
