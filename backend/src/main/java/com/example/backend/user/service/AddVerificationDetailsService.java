e5b55fd4c0c4ade2134fc6f945db72119d8becea
ThinilSandaru <thinilsandaru@gmail.com>
2026-06-11 20:35:36 +0530

    fix

--- backend/src/main/java/com/example/backend/user/service/AddVerificationDetailsService.java
+++ backend/src/main/java/com/example/backend/user/service/AddVerificationDetailsService.java
@@ -74,8 +74,7 @@ public class AddVerificationDetailsService {
 			verification.setNumberOfFamilyMembers(verificationDTO.getNumberOfFamilyMembers());
 			verification.setLongTermHealthIssues(verificationDTO.getLongTermHealthIssues());
 			verification.setRecipientId(recipientId);
-			verification.setAccountNo(verification.getAccountNo());
-			verification.setAccountNo(verification.getAccountNo());
+			verification.setAccountNo(verificationDTO.getAccountNo());
 
 			verification.setDocumentUrl("http://localhost:8080/verifier/get/pdf/"+fileName);
 			verificationRepository.save(verification);
